50fef6dd961a3ff4947cb6c99f954a324cd1ddea
michaelsinanta <michaelchristlambert@gmail.com>
2023-03-01 08:08:39 +0700

    add for deploy

--- project_django/settings.py
+++ project_django/settings.py
@@ -85,6 +85,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'http://localhost:3000',
+    'https://mwitter.vercel.app',
 ]
 
 ROOT_URLCONF = 'project_django.urls'
